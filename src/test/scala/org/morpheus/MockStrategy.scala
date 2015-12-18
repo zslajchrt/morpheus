1a9c43e8238bd79b0e0622aff7361c91cefbf833
zslajchrt <zslajchrt@gmail.com>
2015-12-18 15:02:00 +0100

    thesis: morphing strategies

--- src/test/scala/org/morpheus/MockStrategy.scala
+++ src/test/scala/org/morpheus/MockStrategy.scala
@@ -5,7 +5,7 @@ package org.morpheus
  * Created by zslajchrt on 17/03/15.
  */
 case class MockStrategy[M](alts: Alternatives[M]) extends MorphingStrategy[M] {
-  def chooseAlternatives(instance: MorphKernel[M])(owningMutableProxy: Option[instance.MutableLUB]) = alts
+  def chooseAlternatives(instance: MorphKernel[M])(owningMutableProxy: Option[instance.ImmutableLUB]) = alts
 
 }
 
